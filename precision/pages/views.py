869b4e9864a4692e0919794c211f529e37693b87
Paul Andrei <hey@pauldev.eu>
2017-08-21 01:05:03 +0200

    Add HomeView class

--- precision/pages/views.py
+++ precision/pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateResponseMixin, View
 
-# Create your views here.
+
+class HomeView(TemplateResponseMixin, View):
+    template_name = 'pages/home.html'
+
+    def get(self, request):
+        context = {'section': 'home'}
+        return self.render_to_response(context)
