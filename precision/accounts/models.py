d89e43c649aba78ac9722ca39f9e0c67be0cc897
Paul Andrei <hey@pauldev.eu>
2017-08-21 15:14:52 +0200

    Add an simple abstract user model for school administrators which will be used later

--- precision/accounts/models.py
+++ precision/accounts/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class SchoolAdministrator(AbstractUser):
+    pass
