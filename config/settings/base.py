e322305bb5e30c922e526a5647935920f35b7a8a
Paul Andrei <hey@pauldev.eu>
2017-08-21 14:59:11 +0200

    Add accounts app in installed_apps

--- config/settings/base.py
+++ config/settings/base.py
@@ -51,6 +51,7 @@ THIRD_PARTY_APPS = [
 # Apps specific for this project go here.
 LOCAL_APPS = [
     'precision.pages.apps.PagesConfig',
+    'precision.accounts.apps.AccountsConfig',
 ]
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
