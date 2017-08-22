1e3c43d0e01039b711247eb1db5f3939c4ba89ab
Paul Andrei <hey@pauldev.eu>
2017-08-22 22:07:40 +0200

    Delete custom urls and add urls for built in auth views

--- precision/accounts/urls.py
+++ precision/accounts/urls.py
@@ -1,12 +1,28 @@
 from django.conf.urls import url
-
+from django.contrib.auth.views import login, logout, logout_then_login, 
 from .views import SignInView
 
 
 urlpatterns = [
+
+    # Authentication
+    # ==============
     url(
-        regex=r'^sign-in/$',
-        view=SignInView.as_view(),
-        name='sign_in'
+        regex=r'^login/$',
+        view=login,
+        name='login'
     ),
+
+    url(
+        regex=r'^logout/$',
+        view=logout,
+        name='logout'
+    ),
+
+    url(
+        regex=r'^logout-then-login/$',
+        view=logout_then_login,
+        name='logout_then_login'
+    ),
+
 ]
