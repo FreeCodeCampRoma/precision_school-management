488e29d8b162b502cdd43eb4a84d99a40c8a89a0
Paul Andrei <hey@pauldev.eu>
2017-08-23 13:56:14 +0200

    Add password reset urls

--- precision/accounts/urls.py
+++ precision/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
+from django.contrib.auth.views import (login, logout, logout_then_login,
+                                       password_change, password_change_done, password_reset,
+                                       password_reset_done, password_reset_confirm, password_reset_complete)
 
 from .views import SignInView
 
@@ -19,14 +21,15 @@ urlpatterns = [
         name='logout'
     ),
 
-    # Password Change
-    # ===============
     url(
         regex=r'^logout-then-login/$',
         view=logout_then_login,
         name='logout_then_login'
     ),
 
+
+    # Password Change
+    # ===============
     url(
         regex=r'^password-change/$',
         view=password_change,
@@ -38,4 +41,32 @@ urlpatterns = [
         view=password_change_done,
         name='password_change_done'
     ),
+
+
+    # Password Reset
+    # ==============
+    url(
+        regex=r'^password-reset/$',
+        view=password_reset,
+        name='password_reset',
+    ),
+
+    url(
+        regex=r'^password-reset/done/$',
+        view=password_reset_done,
+        name='password_reset_done'
+    ),
+
+    url(
+        regex=r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$',
+        view=password_reset_confirm,
+        name='password_reset_confirm'
+    ),
+
+    url(
+        regex=r'^password-reset/complete/$',
+        view=password_reset_complete,
+        name='password_reset_complete'
+    ),
+
 ]
