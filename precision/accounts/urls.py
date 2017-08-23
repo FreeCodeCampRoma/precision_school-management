638e9761a6a42a8ab9d8eb7996b0a19d394ad3ea
Paul Andrei <hey@pauldev.eu>
2017-08-23 13:41:01 +0200

    Add password change url patterns

--- precision/accounts/urls.py
+++ precision/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
 from .views import SignInView
 
 
@@ -19,9 +19,23 @@ urlpatterns = [
         name='logout'
     ),
 
+    # Password Change
+    # ===============
     url(
         regex=r'^logout-then-login/$',
         view=logout_then_login,
         name='logout_then_login'
+    ),
+
+    url(
+        regex=r'^password-change/$',
+        view=password_change,
+        name='password_change'
+    ),
+
+    url(
+        regex=r'^password-change/done/$',
+        view=password_change_done,
+        name='password_change_done'
     ),
 ]
